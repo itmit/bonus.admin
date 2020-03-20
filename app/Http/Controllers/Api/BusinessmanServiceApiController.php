8d6c1348dbb4ecc0dc2c8a829dd46ddef9b787b0
Sergey <32011415+leguha@users.noreply.github.com>
2020-03-20 14:37:09 +0300

    Update BusinessmanServiceApiController.php

--- app/Http/Controllers/Api/BusinessmanServiceApiController.php
+++ app/Http/Controllers/Api/BusinessmanServiceApiController.php
@@ -27,7 +27,7 @@ class BusinessmanServiceApiController extends ApiBaseController
     {
         return $this->sendResponse(BusinessmanService::join('service_items', 'businessman_services.service_item_id', '=', 'service_items.id')
         ->where('businessman_services.businessmen_id', auth('api')->user()->id)
-        ->select('businessman_services.uuid', 'service_items.name', 'businessman_services.accrual_method', 'businessman_services.accrual_value')
+        ->select('businessman_services.uuid', 'service_items.name', 'businessman_services.accrual_method', 'businessman_services.accrual_value', 'businessman_services.writeoff_value', 'businessman_services.writeoff_method')
         ->get()
         ->toArray(),'Список созданных услуг');
     }
