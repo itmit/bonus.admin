bde0c0601cb30bf953ae7198a4a0bf1bf8ce56fe
Bronyrus <36816185+Bronyrus@users.noreply.github.com>
2020-04-03 11:31:48 +0300

    Update BusinessmanStockApiController.php

--- app/Http/Controllers/Api/BusinessmanStockApiController.php
+++ app/Http/Controllers/Api/BusinessmanStockApiController.php
@@ -183,9 +183,9 @@ class BusinessmanStockApiController extends ApiBaseController
         
     }
 
-    public function test($uuid)
+    public function test()
     {
-        $yesterday =  mktime(0, 0, 0, date("Y")  , date("m"), date("d")-1);
+        $yesterday = mktime(0, 0, 0, date("Y")  , date("m"), date("d")-1);
         return $this->sendResponse([$yesterday],'time');
         $stocks = Stock::where('expires_at', '<=', $yesterday)->get()->toArray();
         return $this->sendResponse($stocks,'Updated');
