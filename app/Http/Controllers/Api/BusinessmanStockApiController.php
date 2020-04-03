897b62cb1a2a9318a055464e09bf821ed3a31028
Bronyrus <36816185+Bronyrus@users.noreply.github.com>
2020-04-03 12:10:38 +0300

    Update BusinessmanStockApiController.php

--- app/Http/Controllers/Api/BusinessmanStockApiController.php
+++ app/Http/Controllers/Api/BusinessmanStockApiController.php
@@ -185,8 +185,8 @@ class BusinessmanStockApiController extends ApiBaseController
 
     public function test(Request $request)
     {
-        $yesterday = mktime(0, 0, 0, date("Y")  , date("m"), date("d")-1);
+        $yesterday = mktime(0, 0, 0, date("Y"), date("m"), date("d")-1);
         $stocks = Stock::where('expires_at', '<', $yesterday)->get()->toArray();
-        return $this->sendResponse($stocks,'Stocks has been archived');
+        return $this->sendResponse($yesterday, 'Stocks has been archived');
     }
 }
