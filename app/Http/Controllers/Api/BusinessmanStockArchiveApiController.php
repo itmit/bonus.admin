5a162dfde7e5fdf5b95bad3f083e6c9ad12f8357
Bronyrus <36816185+Bronyrus@users.noreply.github.com>
2020-04-03 14:54:27 +0300

    Update BusinessmanStockArchiveApiController.php

--- app/Http/Controllers/Api/BusinessmanStockArchiveApiController.php
+++ app/Http/Controllers/Api/BusinessmanStockArchiveApiController.php
@@ -28,9 +28,9 @@ class BusinessmanStockArchiveApiController extends ApiBaseController
      */
     public function index()
     {
-        return $this->sendResponse(StockArchive::join('service_items', 'stocks.service_id', '=', 'service_items.id')
+        return $this->sendResponse(StockArchive::join('service_items', 'stock_archives.service_id', '=', 'service_items.id')
         ->where('stocks.client_id', auth('api')->user()->id)
-        ->select('stocks.uuid', 'service_items.name AS service_name', 'stocks.name AS name', 'stocks.description', 'stocks.photo', 'stocks.expires_at')
+        ->select('stock_archives.uuid', 'service_items.name AS service_name', 'stock_archives.name AS name', 'stock_archives.description', 'stock_archives.photo', 'stock_archives.expires_at', 'stock_archives.created')
         ->get()
         ->toArray(),'Список акций');
     }
