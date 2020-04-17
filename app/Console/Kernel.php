4cdad2f84f3f8d5ad95bb841ee82b7b9064e88e3
Bronyrus <36816185+Bronyrus@users.noreply.github.com>
2020-04-17 10:18:32 +0300

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\archiveStock;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        archiveStock::class,
     ];
 
     /**
@@ -24,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:archiveStock')->dailyAt('12:00');
+        $schedule->command('command:archiveStock')->everyTenMinutes();
     }
 
     /**
--- app/Http/Controllers/Api/ClientApiController.php
+++ app/Http/Controllers/Api/ClientApiController.php
@@ -77,7 +77,47 @@ class ClientApiController extends ApiBaseController
      */
     public function show($id)
     {
-        //
+        $client = Client::where('id', auth('api')->user()->id)->first();
+        $result = [];
+        if($client->type == 'businessman')
+        {
+            $info = ClientBusinessman::where('client_id', $client->id)->first();
+            $result = [
+                'uuid' => $client->uuid,
+                'email' => $client->email,
+                'phone' => $client->phone,
+                'login' => $client->login,
+                'name' => $client->name,
+                'type' => $client->type,
+                'country' => $info->country,
+                'city' => $info->city,
+                'address' => $info->address,
+                'work_time' => $info->work_time,
+                'contact' => $info->contact,
+                'description' => $info->description,
+                'photo' => $info->photo,
+            ];
+        }
+        if($client->type == 'customer')
+        {
+            $info = ClientCustomer::where('client_id', $client->id)->first();
+            $result = [
+                'uuid' => $client->uuid,
+                'email' => $client->email,
+                'phone' => $client->phone,
+                'login' => $client->login,
+                'name' => $client->name,
+                'type' => $client->type,
+                'country' => $info->country,
+                'city' => $info->city,
+                'sex' => $info->sex,
+                'birthday' => $info->birthday,
+                'car' => $info->car,
+                'photo' => $info->photo,
+            ];
+        }
+        
+        return $this->sendResponse($result,'Клиент');
     }
 
     /**
@@ -88,7 +128,47 @@ class ClientApiController extends ApiBaseController
      */
     public function edit($id)
     {
-        //
+        $client = Client::where('id', auth('api')->user()->id)->first();
+        $result = [];
+        if($client->type == 'businessman')
+        {
+            $info = ClientBusinessman::where('client_id', $client->id)->first();
+            $result = [
+                'uuid' => $client->uuid,
+                'email' => $client->email,
+                'phone' => $client->phone,
+                'login' => $client->login,
+                'name' => $client->name,
+                'type' => $client->type,
+                'country' => $info->country,
+                'city' => $info->city,
+                'address' => $info->address,
+                'work_time' => $info->work_time,
+                'contact' => $info->contact,
+                'description' => $info->description,
+                'photo' => $info->photo,
+            ];
+        }
+        if($client->type == 'customer')
+        {
+            $info = ClientCustomer::where('client_id', $client->id)->first();
+            $result = [
+                'uuid' => $client->uuid,
+                'email' => $client->email,
+                'phone' => $client->phone,
+                'login' => $client->login,
+                'name' => $client->name,
+                'type' => $client->type,
+                'country' => $info->country,
+                'city' => $info->city,
+                'sex' => $info->sex,
+                'birthday' => $info->birthday,
+                'car' => $info->car,
+                'photo' => $info->photo,
+            ];
+        }
+        
+        return $this->sendResponse($result,'Клиент');
     }
 
     /**
@@ -100,7 +180,32 @@ class ClientApiController extends ApiBaseController
      */
     public function update(Request $request, $id)
     {
-        //
+        $client = Client::where('id', $id)->first();
+        if($client->type == 'businessman')
+        {
+            ClientBusinessman::where('client_id', $id)->update([
+                'country' => $request->country,
+                'city' => $request->city,
+                'address' => $request->address,
+                'work_time' => $request->work_time,
+                'contact' => $request->contact,
+                'description' => $request->description,
+                'photo' => $request->photo,
+            ]);   
+        }
+        if($client->type == 'customer')
+        {
+            ClientCustomer::where('client_id', $id)->update([
+                'country' => $request->country,
+                'city' => $request->city,
+                'sex' => $request->sex,
+                'birthday' => $request->birthday,
+                'car' => $request->car,
+                'photo' => $request->photo,
+            ]);
+        }
+        
+        return $this->sendResponse([],'Обновлено');
     }
 
     /**
